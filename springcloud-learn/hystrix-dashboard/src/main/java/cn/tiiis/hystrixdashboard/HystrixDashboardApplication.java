327fba6f67db642adc6224684daf82e29d082a4d
tzpBingo <tzp_Bingo@foxmail.com>
2018-02-02 17:42:05 +0800

    add

---
+++ springcloud-learn/hystrix-dashboard/src/main/java/cn/tiiis/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,14 @@
+package cn.tiiis.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard  //启用Hystrix仪表盘功能
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}
---
+++ springcloud-learn/ribbon-consumer/src/main/java/cn/tiiis/ribbonconsumer/RibbonConsumerApplication.java
@@ -0,0 +1,26 @@
+package cn.tiiis.ribbonconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@EnableCircuitBreaker  //开启断路器功能
+@EnableDiscoveryClient  //表明该应用是Eureka客户端
+@SpringBootApplication
+public class RibbonConsumerApplication {
+
+	@Bean
+	@LoadBalanced  //注解开启客户端负载均衡
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonConsumerApplication.class, args);
+	}
+}
