3b04963e4323ffb995ce3a3b6733db9b0310f00a
tzpBingo <tzp_Bingo@foxmail.com>
2018-04-12 11:25:22 +0800

    add

---
+++ go-learn/src/leetcode/Algorithms/0137.single-number-ii/single-number-ii.go
@@ -0,0 +1,19 @@
+package Problem0137
+
+// 假设 nums 中的数字为 32 bit
+func singleNumber(nums []int) int {
+	n := len(nums)
+	res := 0
+
+	var i uint
+	for ; i < 64; i++ {
+		temp := 0
+		for j := 0; j < n; j++ {
+			temp += nums[j] >> i & 1 //首先把输入数字的第i位加起来。
+		}
+		temp %= 3        //然后求它们除以3的余数。
+		res |= temp << i //把二进制表示的结果转化为十进制表示的结果
+	}
+
+	return res
+}
---
+++ go-learn/src/leetcode/Algorithms/0148.sort-list/sort-list.go
@@ -0,0 +1,72 @@
+package Problem0148
+
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
+type ListNode = kit.ListNode
+
+func sortList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	secHead := split(head)
+	return merge(sortList(head), sortList(secHead))
+}
+
+// 从中间位置，切分 list
+func split(head *ListNode) *ListNode {
+	// head.Next != nil
+	// 因为， sortList 已经帮忙检查过了
+
+	// fast 的变化速度是 slow 的两倍
+	// 当 fast 到达末尾的时候，slow 正好在 list 的中间
+	slow, fast := head, head
+	var tail *ListNode
+	for fast != nil && fast.Next != nil {
+		tail = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	// 斩断 list
+	tail.Next = nil
+	return slow
+}
+
+// 把已经排序好了的两个 list left 和 right
+// 进行合并
+func merge(left, right *ListNode) *ListNode {
+	// left != nil , right != nil
+	// 因为， sortList 已经帮忙检查过了
+
+	var head, cur, pre *ListNode
+	for left != nil && right != nil {
+		// cur 指向 left 和 right 中最小的节点
+		if left.Val < right.Val {
+			cur = left
+			left = left.Next
+		} else {
+			cur = right
+			right = right.Next
+		}
+		// 生成 head 节点
+		// 或者 链接 pre 节点
+		if head == nil {
+			head = cur
+		} else {
+			pre.Next = cur
+		}
+		// 移动 pre 节点
+		pre = cur
+	}
+
+	// 处理 left 或 right 中，剩下的节点
+	if left == nil {
+		pre.Next = right
+	} else {
+		pre.Next = left
+	}
+
+	return head
+}
