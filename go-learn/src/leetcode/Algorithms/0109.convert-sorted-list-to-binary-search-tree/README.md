0a5dc420d8f2a8bc442e087a866e8628a1af4e32
tzpBingo <tzp_Bingo@foxmail.com>
2018-04-12 11:24:55 +0800

    add

---
+++ go-learn/src/leetcode/Algorithms/0109.convert-sorted-list-to-binary-search-tree/README.md
@@ -0,0 +1,12 @@
+# [109. Convert Sorted List to Binary Search Tree](https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/)
+
+## 题目
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+## 解题思路
+
+见程序注释
+
+又见 100% ，感谢 LeetCode 服务器。
+
+![100%](109.png)
---
+++ go-learn/src/leetcode/Algorithms/0109.convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
@@ -0,0 +1,35 @@
+package Problem0109
+
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
+type ListNode = kit.ListNode
+type TreeNode = kit.TreeNode
+
+func sortedListToBST(head *ListNode) *TreeNode {
+	return sortedArrayToBST(list2Slice(head))
+}
+
+func list2Slice(head *ListNode) []int {
+	res := []int{}
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	mid := len(nums) / 2
+	return &TreeNode{
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
+	}
+}
